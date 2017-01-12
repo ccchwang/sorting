18c7de7f1a1cf777d13683235e0bcc15518f64c2
Chloe Hwang <c.chloeh@gmail.com>
2017-01-12 15:46:40 -0500

    working on merge

--- bubblesort.js
+++ bubblesort.js
@@ -1,5 +1,25 @@
-function bubbleSort(array){
+function bubbleSort(array) {
+  if (array.length === 0 || array.length === 1) {
+    return array;
+  }
+  var i=0;
 
-
-
+  while (i<array.length) {
+    for (var j=0; j<array.length-1; j++) {
+      if (array[j] > array[j+1]) {
+        swap(array[j], array[j+1], array, j)
+      }
+    }
+    i++
+  }
+return array;
 }
+
+// hold onto bigger value
+
+var swap = function (val1, val2, array, j) {
+  array[j] = val2;
+  array[j+1] = val1;
+  return array;
+}
+
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,26 @@
 describe('Bubble Sort', function(){
+  beforeEach(function() {
+    var array = [5, 2, 4, 1, 3];
+  })
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('sorts the values', function() {
+    expect(bubbleSort([5, 2, 4, 1, 3])).toEqual([1, 2, 3, 4, 5])
+  });
+  it('sorts the values', function() {
+    expect(bubbleSort([7, 5, 3, 1])).toEqual([1, 3, 5, 7])
+  });
+});
+
+describe('Swap', function() {
+  beforeEach(function() {
+    spyOn(window, 'swap').and.callThrough();
+    bubbleSort([7, 5, 3, 1]);
+  })
+
+  it('how many times swapped', function() {
+    expect(window.swap.calls.count()).toEqual(6);
   });
 });
